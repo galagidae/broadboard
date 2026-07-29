35fecfcb3be9606fe619f8f390daa0c26ed2ec31
Anthony Benbrook <anthonybenbrook@hotmail.com>
2026-07-29 21:27:23 +0100

    Adjust minification

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,10 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
         create ("playbundle") {
             initWith(getByName("release"))
